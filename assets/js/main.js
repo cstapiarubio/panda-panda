36069f2b8055383d8febb87ce4f78171b5de8f86
cstapiarubio <cstapiarubio@gmail.com>
2017-05-22 12:55:35 -0400

    al fin se borra la primera imagen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,20 @@
-var foto1=document.getElementById('img_1');
-var contenedor=document.createElement("div");
-var imagenes=document.createElement("div");
-imagenes.appendChild(img_1);
-contenedor.appendChild(imagenes);
+
+var fotos=document.getElementById("img_1");
 
 
 
-foto1.addEventListener("click", function(){
-	contenedor.removeChild(imagenes);
+fotos.addEventListener("click", function(){
+	fotos.remove("img_1");
 });
 
 /*document.addEventListener("click", function(){
     document.getElementById("img_1").innerHTML ="hola";
 });
-
-
-
+imagenes.appendChild(foto1);
+var imagenes=document.createElement("div");
+var photos=fotos.classList("imagenes");
+contenedor.appendChild(imagenes);
+var contenedor=document.createElement("div");
 img_1.addEventListener("click", function(){
 contenedor.removeChild(img_1);
 })*/
