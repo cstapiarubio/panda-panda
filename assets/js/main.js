2c6e3987de4fc4b2d6f348ede429f63ecdbc1042
cstapiarubio <cstapiarubio@gmail.com>
2017-05-21 23:57:37 -0400

    agregue funcion addeventlistener pero no funciona

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-main.js
+/*var foto1=document.getElementsByClassName('img_1');
+foto1.addEventListener("click", function(){
+	
+})*/
+
+document.addEventListener("click", function(){
+    document.getElementById("img_1").innerHTML ="hola";
+});
