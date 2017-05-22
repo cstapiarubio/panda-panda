182e2da7ba0b9e8de932844b7c3d6e0b093e38a0
cstapiarubio <cstapiarubio@gmail.com>
2017-05-22 11:53:36 -0400

    intentos en js de borrar las fotos de pandas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,21 @@
-/*var foto1=document.getElementsByClassName('img_1');
-foto1.addEventListener("click", function(){
-	
-})*/
+var foto1=document.getElementById('img_1');
+var contenedor=document.createElement("div");
+var imagenes=document.createElement("div");
+imagenes.appendChild(img_1);
+contenedor.appendChild(imagenes);
 
-document.addEventListener("click", function(){
+
+
+foto1.addEventListener("click", function(){
+	contenedor.removeChild(imagenes);
+});
+
+/*document.addEventListener("click", function(){
     document.getElementById("img_1").innerHTML ="hola";
 });
+
+
+
+img_1.addEventListener("click", function(){
+contenedor.removeChild(img_1);
+})*/
